31cb40189275509343bfac6fe1f82b760d0543b2
Polljii143 <paul@reminisense.com>
2016-11-29 12:51:28 +0800

    Filter By Category

--- FeatherQ/FQCategoriesCollectionViewController.swift
+++ FeatherQ/FQCategoriesCollectionViewController.swift
@@ -69,15 +69,19 @@ class FQCategoriesCollectionViewController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
+        if segue.identifier == "filterByCategory" {
+            let destView = segue.destination as! FQSearchTableViewController
+            if let selectedCell = sender as? FQCategoriesCollectionViewCell {
+                destView.chosenCategory = selectedCell.categoryTitle.text!
+            }
+        }
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
--- FeatherQ/FQSearchTableViewController.swift
+++ FeatherQ/FQSearchTableViewController.swift
@@ -13,8 +13,10 @@ import SwiftSpinner
 
 class FQSearchTableViewController: UITableViewController, UISearchResultsUpdating {
     
-    var businessSearch = UISearchController()
+    var filterSearch = UISearchController()
     var businessList = [FQBusiness]()
+    var filteredBusinesses = [String]()
+    var chosenCategory = "All"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,11 +27,11 @@ class FQSearchTableViewController: UITableViewController, UISearchResultsUpdatin
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
-        self.businessSearch = UISearchController(searchResultsController: nil)
-        self.businessSearch.searchResultsUpdater = self
-        self.businessSearch.dimsBackgroundDuringPresentation = false
-        self.businessSearch.searchBar.sizeToFit()
-        self.tableView.tableHeaderView = self.businessSearch.searchBar
+        self.filterSearch = UISearchController(searchResultsController: nil)
+        self.filterSearch.searchResultsUpdater = self
+        self.filterSearch.dimsBackgroundDuringPresentation = false
+        self.filterSearch.searchBar.sizeToFit()
+        self.tableView.tableHeaderView = self.filterSearch.searchBar
         self.tableView.reloadData()
     }
 
@@ -51,6 +53,9 @@ class FQSearchTableViewController: UITableViewController, UISearchResultsUpdatin
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        if self.filterSearch.isActive {
+            return self.filteredBusinesses.count
+        }
         return self.businessList.count
     }
 
@@ -58,17 +63,22 @@ class FQSearchTableViewController: UITableViewController, UISearchResultsUpdatin
         let cell = tableView.dequeueReusableCell(withIdentifier: "FQSearchTableViewCell", for: indexPath) as! FQSearchTableViewCell
 
         // Configure the cell...
-        cell.businessName.text = self.businessList[indexPath.row].name!
-        cell.categoryName.text = self.businessList[indexPath.row].category!
-        cell.address.text = self.businessList[indexPath.row].address!
-        cell.peopleInLine.text = self.businessList[indexPath.row].people_in_line!
-        cell.waitingTIme.text = self.businessList[indexPath.row].serving_time!
+        let listData = self.businessListingDetails(index: indexPath.row)
+        cell.businessName.text = listData["name"]
+        cell.categoryName.text = listData["category"]
+        cell.address.text = listData["address"]
+        cell.peopleInLine.text = listData["people_in_line"]
+        cell.waitingTIme.text = listData["serving_time"]
 
         return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140.0
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.filterSearch.isActive = false
     }
 
     /*
@@ -108,7 +118,15 @@ class FQSearchTableViewController: UITableViewController, UISearchResultsUpdatin
     
     @available(iOS 8.0, *)
     public func updateSearchResults(for searchController: UISearchController) {
-        
+        var unwrappedBusinessNames = [String]()
+        for business in self.businessList {
+            unwrappedBusinessNames.append(business.name!+"|"+business.category!+"|"+business.address!+"|"+business.people_in_line!+"|"+business.serving_time!+"|"+business.key!)
+        }
+        self.filteredBusinesses.removeAll(keepingCapacity: false)
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (unwrappedBusinessNames as NSArray).filtered(using: searchPredicate)
+        self.filteredBusinesses = array as! [String]
+        self.tableView.reloadData()
     }
 
     /*
@@ -123,7 +141,6 @@ class FQSearchTableViewController: UITableViewController, UISearchResultsUpdatin
     
     func postDisplayBusinesses() {
         SwiftSpinner.show("Fetching..")
-        self.businessList.removeAll()
         Alamofire.request(Router.postDisplayBusinesses).responseJSON { response in
             if response.result.isFailure {
                 debugPrint(response.result.error!)
@@ -135,13 +152,41 @@ class FQSearchTableViewController: UITableViewController, UISearchResultsUpdatin
             }
             let responseData = JSON(data: response.data!)
             debugPrint(responseData)
+            self.businessList.removeAll()
             for business in responseData {
                 let dataObj = business.1.dictionaryObject!
-                self.businessList.append(FQBusiness(modelAttr: dataObj))
+                if self.chosenCategory != "All" {
+                    if dataObj["category"] as! String == self.chosenCategory {
+                        self.businessList.append(FQBusiness(modelAttr: dataObj))
+                    }
+                }
+                else {
+                    self.businessList.append(FQBusiness(modelAttr: dataObj))
+                }
             }
             self.tableView.reloadData()
             SwiftSpinner.hide()
         }
+    }
+    
+    func businessListingDetails(index: Int) -> [String:String] {
+        var listData = [String:String]()
+        if self.filterSearch.isActive {
+            let businessData = self.filteredBusinesses[index].components(separatedBy: "|")
+            listData["name"] = businessData[0]
+            listData["category"] = businessData[1]
+            listData["address"] = businessData[2]
+            listData["people_in_line"] = businessData[3]
+            listData["serving_time"] = businessData[4]
+        }
+        else {
+            listData["name"] = self.businessList[index].name!
+            listData["category"] = self.businessList[index].category!
+            listData["address"] = self.businessList[index].address!
+            listData["people_in_line"] = self.businessList[index].people_in_line!
+            listData["serving_time"] = self.businessList[index].serving_time!
+        }
+        return listData
     }
     
 }
